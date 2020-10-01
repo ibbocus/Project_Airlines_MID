bd92f82582c0329159f61e659acfe52fb419dce0
ibbocus <ib_bocus@hotmail.com>
2020-10-01 13:50:54 +0100

    added files

---
+++ Project_MID_Airline/Run/run.py
@@ -0,0 +1,6 @@
+from Project_MID_Airline.Run.gui import *
+
+app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
+window = Ui()  # Create an instance of our class
+app.exec_()  # Start the application
+
